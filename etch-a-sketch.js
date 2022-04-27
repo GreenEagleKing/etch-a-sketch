a78809d729468960cc0c071e53d1c7fd4ae44198
Ben King <benking1293@gmail.com>
2022-04-27 11:43:29 +0100

    Allows for custom grid input. When user inputs a number the grid resets to user choosen size.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,10 +1,19 @@
-// Create 16x16 grid
-
 let gridContainer = document.querySelector("#gridCanvas");
 const button = document.querySelector("#resetBtn");
 button.addEventListener("click", resetColor);
 
+const userBtn = document.querySelector("#userBtn");
+userBtn.addEventListener("click", () => {
+    resetColor();
+    resetGrid();
+    customGrid();
+    boxListeners();
+});
+
+// Create 16x16 grid
+
 function createGrid(numBox) {
+    console.log(numBox)
     for (let i = 0; i < numBox; i++)    {
         let div = document.createElement("div");
         for (let j = 0; j < numBox; j++)    {
@@ -38,4 +47,22 @@ function resetColor() {
     gridBox.forEach(element => {
     element.classList.remove("colorChange");
 })
+}
+
+// resets grid when new customGrid is created
+
+function resetGrid()    {
+    let gridBox = document.querySelectorAll(".box").forEach(el => el.remove());
+}
+
+// create new grid from user input between 1 and 100
+
+function customGrid()   {
+    let userInput = parseInt(document.querySelector("#userGridInput").value);
+    if (userInput < 0 || userInput > 100)   {
+        alert("Please enter a valid number between 1 - 100");
+        createGrid(16);
+    }   else {
+        createGrid(userInput);
+    }
 }
